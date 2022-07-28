a14a64911c56eaf1fa8f9cd7643b5ec2fe193763
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2022-07-28 20:41:57 +0900

    ogpカードのアップデートとか使用スクリプトとかの更新

---
+++ content-update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+tcardgen -f ./../../../tcardgen/font \
+           -o . \
+           -c ./../../../tcardgen/config.yaml \
+           -t ./../../../tcardgen/template-img.png \
+           ./index.md
+git add . && git commit -m "update contents" && git push
+cd ../../../ && git add contents && git commit -m "update diary from submodule" && git push
